6649775f68cba458475340602c71eec30725095c
김희주 <hejoo345@naver.com>
2021-10-25 18:43:25 +0900

    Add: 문제 풀이

---
+++ 프로그래머스/2016년.js
@@ -0,0 +1,13 @@
+function solution(a, b) {
+    let answer = 0;
+    const day = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
+    const month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    for(let i = 0; i < a - 1; i++){
+        answer += month[i]
+    }
+    answer += b + 4; // 2016.01.01은 금요일
+    answer %= 7;
+    return day[answer];
+}
+
+solution()
