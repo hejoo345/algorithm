95cfda151d38ecb9438e418e822148ffe8000244
김희주 <67685741+hejoo345@users.noreply.github.com>
2023-10-29 16:22:41 +0900

    [level 3] Title: 섬 연결하기, Time: 0.53 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    static int[] parent;
+
+    public int find(int a) {
+        if (a == parent[a])
+            return a;
+        else
+            return parent[a] = find(parent[a]);
+    }
+
+    public void union(int a, int b) {
+        int p1 = find(a);
+        int p2 = find(b);
+        if (p1 < p2) {
+            parent[p2] = p1;
+        } else {
+            parent[p1] = p2;
+        }
+    }
+
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        Arrays.sort(costs, (o1, o2) -> {
+            return o1[2] - o2[2];
+        });
+
+        for (int i = 0; i < costs.length; i++) {
+            if (find(costs[i][0]) != find(costs[i][1])) {
+                union(costs[i][0], costs[i][1]);
+                answer += costs[i][2];
+            }
+        }
+        return answer;
+    }
+
+}
