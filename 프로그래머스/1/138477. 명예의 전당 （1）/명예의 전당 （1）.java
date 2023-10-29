11e9534071a5aa7feb231b727bf143685aa2bf6d
김희주 <67685741+hejoo345@users.noreply.github.com>
2023-10-29 17:06:39 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.39 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -3,16 +3,14 @@ import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
-        List<Integer> list = new ArrayList<>();
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
 
         for (int i = 0; i < score.length; i++) {
-            list.add(score[i]);
-            Collections.sort(list, Collections.reverseOrder());
-            if (i < k) {
-                answer[i] = list.get(i);
-            } else {
-                answer[i] = list.get(k - 1);
+            queue.add(score[i]);
+            if (i > k - 1) {
+                queue.poll();
             }
+            answer[i] = queue.peek();
         }
         return answer;
     }
