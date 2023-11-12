59ba685a4274c4854b8d7791bc0f512a1b58f6d8
김희주 <67685741+hejoo345@users.noreply.github.com>
2023-11-12 21:08:29 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,19 +1,11 @@
 function solution(left, right) {
     let answer = 0;
     for(let i = left; i <= right; i++) {
-        if(func(i) % 2 == 0) { // 약수의 개수가 짝수라면 더하고
-            answer += i;
-        } else { // 홀수라면 뺀다
+        if(Number.isInteger(Math.sqrt(i))) { // 제곱근이 정수라면 약수의 개수는 홀수
             answer -= i;
+        } else { 
+            answer += i;
         }
     }
     return answer;
-}
-function func(num) {
-    let cnt = 0;
-    for(let i = 0; i * i <= num; i++){
-        if(i * i == num) cnt++;
-        else if(num % i == 0) cnt += 2;
-    }
-    return cnt;
 }
