f9257da5c849e3e47abde821b64174bfa581d47c
김희주 <67685741+hejoo345@users.noreply.github.com>
2023-11-13 23:19:20 +0900

    [level 1] Title: 옹알이 (2), Time: 0.43 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -2,7 +2,6 @@ function solution(babbling) {
     const words = ["aya", "ye", "woo", "ma"];
     let answer = 0;
     babbling.forEach(word => {
-        let cnt = 0;
         for (let i = 0; i < 4; i++) {
             while(word.includes(words[i])) {
                 // 연속해서 같은 발음을 하는지 확인
@@ -10,12 +9,11 @@ function solution(babbling) {
                 const ck = word.slice(idx + words[i].length, idx + (words[i].length * 2));
                 if (words[i] == ck) break;
                 else {
-                    cnt++;
-                    word = word.replace(words[i], "_");
+                    word = word.replace(words[i], " ");
                 }
             }
         }
-        if (word == "_".repeat(cnt)) answer++;
+        if (word.replaceAll(" ","") == "") answer++;
     })
     return answer;
 }
