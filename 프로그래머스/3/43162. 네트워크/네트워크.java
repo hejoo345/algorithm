528b44eb10ac1dba7b91a0f6285cbc2762c5301f
김희주 <67685741+hejoo345@users.noreply.github.com>
2024-01-04 09:04:29 +0900

    [level 3] Title: 네트워크, Time: 0.10 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,27 +2,27 @@ class Solution {
     static int n;
     static int[][] computers;
     static boolean[] visited;
-
-    public int solution(int cn, int[][] ccomputers) {
-        n = cn;
-        computers = ccomputers;
+    static int answer;
+    
+    public int solution(int num, int[][] computer) {
+        n = num;
+        computers = computer;
         visited = new boolean[n];
-        int answer = 0;
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                answer++;
+        answer = 0;
+        for(int i = 0; i < n; i++) {
+            if(!visited[i]){
                 dfs(i);
+                answer++;
             }
         }
         return answer;
     }
-
+    
     public void dfs(int node) {
-        if (visited[node]) return;
-
         visited[node] = true;
-        for (int i = 0; i < n; i++) {
-            if (computers[node][i] == 1) {
+        
+        for(int i = 0; i < n; i++) {
+            if(!visited[i] && computers[node][i] == 1) {
                 dfs(i);
             }
         }
