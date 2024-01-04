0042b41b423df5f0d295456e953481ece13f0064
김희주 <67685741+hejoo345@users.noreply.github.com>
2024-01-04 10:23:22 +0900

    [level 3] Title: 입국심사, Time: 0.55 ms, Memory: 69.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -3,16 +3,17 @@ import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
         Arrays.sort(times);
-        long answer = 0;
-        
-        long start = 1;
-        long end = (long)n * times[times.length - 1];
+
+        long start = times[0];
+        long end = (long) n * times[times.length - 1];
+        long answer = end;
 
         while (start <= end) {
             long mid = (start + end) / 2;
             long cnt = 0; // 심사 가능한 사람 수
-            for (int i : times) {
-                cnt += mid / i; // 각 심사관이 심사할 수 있는 사람 수
+            for (int t : times) {
+                cnt += mid / t; // 각 심사관이 심사할 수 있는 사람 수
+                if(cnt >= n) break;
             }
 
             if (cnt < n) {
