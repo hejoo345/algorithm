dc2bb2447adac0b1dba9c67ba69ff31a8f723956
김희주 <67685741+hejoo345@users.noreply.github.com>
2024-01-05 09:10:22 +0900

    [level 2] Title: 프로세스, Time: 0.74 ms, Memory: 67.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,30 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        int answer = 0; // 순서
-        Queue<Integer> queue = new LinkedList<>(); // 중요도 큐
-        Queue<Integer> idxqueue = new LinkedList<>(); // 인덱스 큐
-        for (int i = 0; i < priorities.length; i++) {
+        int answer = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        Queue<Integer> idxQueue = new LinkedList<>();
+        for(int i = 0; i < priorities.length; i++) {
             queue.add(priorities[i]);
-            idxqueue.add(i);
+            idxQueue.add(i);
         }
-        while (!queue.isEmpty()) {
-            int peek = queue.peek();
-            boolean ck = false;
-            for (int n : queue) {
-                if (n > peek) { // 우선순위가 더 큰 프로세스가 있다면
+        
+        while(!queue.isEmpty()) {
+            int curr = queue.poll();
+            int curr_idx = idxQueue.poll();
+            boolean ck = false; // 뒤에 더 큰 우선순위가 있는지 확인
+            for(int p: queue) {
+                if(curr < p) {
                     ck = true;
                     break;
                 }
             }
-            int tmp = queue.poll(); // 중요도 큐 맨 앞의 값 뽑기
-            int idx = idxqueue.poll(); // 인덱스 큐 맨 앞의 값 뽑기
-            if (ck) {
-                queue.add(tmp);
-                idxqueue.add(idx);
+            if(ck) {
+                queue.add(curr);
+                idxQueue.add(curr_idx);
             } else {
                 answer++;
-                if (idx == location) {
+                if(curr_idx == location) {
                     break;
                 }
             }
