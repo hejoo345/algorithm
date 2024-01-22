92fd7331ddaabdb9926c2c2e41c0a1f7305c1dad
김희주 <67685741+hejoo345@users.noreply.github.com>
2024-01-22 09:08:37 +0900

    [level 4] Title: 도둑질, Time: 24.90 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -3,19 +3,24 @@ import java.util.*;
 class Solution {
     public int solution(int[] money) {
         int n = money.length;
-        int[] dp1 = new int[n]; // 첫번째 집을 털었을 때
+        int answer = 0;
+
+        // 첫번째 집 털었을 때
+        int[] dp1 = new int[n];
         dp1[0] = money[0];
         dp1[1] = money[0];
         for (int i = 2; i < n - 1; i++) {
-            dp1[i] = Math.max(dp1[i - 1], money[i] + dp1[i - 2]);
+            dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + money[i]);
         }
 
-        int[] dp2 = new int[n]; // 첫번째 집 안털었을 때
+        // 두번째 집부터 털었을 때
+        int[] dp2 = new int[n];
         dp2[1] = money[1];
         for (int i = 2; i < n; i++) {
-            dp2[i] = Math.max(dp2[i - 1], money[i] + dp2[i - 2]);
+            dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + money[i]);
         }
-        int answer = Math.max(dp1[n - 2], dp2[n - 1]);
+
+        answer = Math.max(dp1[n - 2], dp2[n - 1]);
         return answer;
     }
 }
